8a139a18abf086b6ceea3efa6c2d668c07aa609c
João Henrique <jhenrmm@gmail.com>
2025-02-24 13:59:45 -0300

    Learned about switch cases

--- src/academy/devdojo/maratonajava/introdução/Aula05.java
+++ src/academy/devdojo/maratonajava/introdução/Aula05.java
@@ -2,15 +2,29 @@ package academy.devdojo.maratonajava.introdução;
 
 public class Aula05 {
     public static void main(String[] args) {
-        int age = -12;
-        if (age >= 18) {
-            System.out.println("Allowed");
-        }
-        else if(age < 0){
-            System.out.println("Invalid");
-        }
-        else {
-            System.out.println("Prohibited");
+        double salary = 5000;
+        String doar = "Posso doar";
+        String naoDoar = "Não posso doar";
+        float despesa = 4000F;
+        String resultado = "";
+        salary -= despesa;
+        resultado = salary >= 1000 ? doar : naoDoar;
+        System.out.println(resultado);
+        /*
+        byte dia = 5
+        switch(dia);
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça");
+                break;
+            default:
+                System.out.println("Opção Inválida")
+                break;
+         */
         }
     }
-}
