3e84d31ec665eb253c752946ceff8355d0917491
João Henrique <jhenrmm@gmail.com>
2025-03-02 20:53:29 -0300

    Learned about Constructors

--- src/javacore/construtores/dominio/dominio/Anime.java
+++ src/javacore/construtores/dominio/dominio/Anime.java
@@ -1,25 +1,27 @@
-package javacore.sobrecargametodos.dominio;
+package javacore.construtores.dominio.dominio;
 
 public class Anime {
     private String nome;
     private String tipo;
     private int episodios;
     private String genero;
+    private String estudio;
 
-    public Anime(){
-
-    }
-
-
-    public void init(String nome, String tipo, int episodios){
+    public Anime(String nome, String tipo, int episodios, String genero) {
+        this(); // chama o outro construtor Anime()
         this.nome = nome;
         this.tipo = tipo;
         this.episodios = episodios;
+        this.genero = genero;
     }
 
-    public void init(String nome, String tipo, int episodios, String genero){
-        this.init(nome, tipo, episodios);
-        this.genero = genero;
+    public Anime(String nome, String tipo, int episodios, String genero, String estudio){
+        this(nome, tipo, episodios, genero);
+        this.estudio = estudio;
+    }
+
+    public Anime(){
+        System.out.println("Dentro do construtor sem argumentos");
     }
 
     public void imprime(){
@@ -27,6 +29,7 @@ public class Anime {
         System.out.println(this.tipo);
         System.out.println(this.episodios);
         System.out.println(this.genero);
+        System.out.println(this.estudio);
     }
 
     public void setNome(String nome){
