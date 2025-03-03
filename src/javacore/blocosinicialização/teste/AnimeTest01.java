5763194c88b83992176b43292912feabbb0dfa3d
João Henrique <jhenrmm@gmail.com>
2025-03-03 20:01:51 -0300

    Learned about Inicialization Blocks

---
+++ src/javacore/blocosinicialização/teste/AnimeTest01.java
@@ -0,0 +1,12 @@
+package javacore.blocosinicialização.teste;
+
+import javacore.blocosinicialização.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("One Piece");
+        for (int episodio : anime.getEpisodios()){
+            System.out.print(episodio + " ");
+        }
+    }
+}
