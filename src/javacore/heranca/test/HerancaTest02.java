427e75a9a07211569a08c0bb2ee24b203a5b6ed9
João Henrique <jhenrmm@gmail.com>
2025-03-09 17:28:56 -0300

    Learned about Inheritance and Method Overriding

---
+++ src/javacore/heranca/test/HerancaTest02.java
@@ -0,0 +1,9 @@
+package javacore.heranca.test;
+
+import javacore.heranca.dominio.Funcionario;
+
+public class HerancaTest02 {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario("Jorge");
+    }
+}
---
+++ src/javacore/sobrescrita/teste/AnimeTest01.java
@@ -0,0 +1,10 @@
+package javacore.sobrescrita.teste;
+
+import javacore.sobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Bleach");
+        System.out.println(anime.toString());
+    }
+}
