7cf90884afdb983eae98870d82240f6391ddf883
João Henrique <jhenrmm@gmail.com>
2025-03-13 14:08:32 -0300

    Learned About Interface

---
+++ src/javacore/Minterface/dominio/DataBaseLoader.java
@@ -0,0 +1,21 @@
+package javacore.Minterface.dominio;
+
+public class DataBaseLoader implements DataLoader, DataRemover{
+    @Override
+    public void load() {
+        System.out.println("Carregando Dados do Banco de Dados");
+    }
+    @Override
+    public void remove() {
+        System.out.println("Removendo Dados do Banco de Dados");
+    }
+
+    @Override
+    public void checkPermission() {
+        System.out.println("Checando Permissões no Banco de Dados");
+    }
+
+    public static void retrieveMaxDataSize(){
+        System.out.println("Dentro da Classe DataBaseLoader");
+    }
+}
---
+++ src/javacore/Minterface/dominio/FileLoader.java
@@ -0,0 +1,18 @@
+package javacore.Minterface.dominio;
+
+public class FileLoader implements DataLoader, DataRemover {
+    @Override
+    public void load() {
+        System.out.println("Carregando Dados do Arquivo");
+    }
+
+    @Override
+    public void remove() {
+        System.out.println("Removendo Dados do Banco de Dados");
+    }
+
+    @Override
+    public void checkPermission() {
+        System.out.println("Checando Permissões no Arquivo");
+    }
+}
