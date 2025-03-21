92ce525604f3b552b96652e4f8d36ab8b2e2f2f3
João Henrique <jhenrmm@gmail.com>
2025-03-21 16:02:01 -0300

    Learned about Strings types

--- src/javacore/exception/exception/test/SobrescritaComExceptionTest01.java
+++ src/javacore/exception/exception/test/SobrescritaComExceptionTest01.java
@@ -15,6 +15,5 @@ public class SobrescritaComExceptionTest01 {
         } catch (LoginInvalidoException | FileNotFoundException e){
             e.printStackTrace();
         }
-
     }
 }
---
+++ src/javacore/string/test/StringBuilderTest01.java
@@ -0,0 +1,16 @@
+package javacore.string.test;
+
+public class StringBuilderTest01 {
+    public static void main(String[] args) {
+        String nome = "William Suane";
+        nome.concat(" DevDojo");
+        nome = nome.substring(0, 3);
+        System.out.println(nome);
+        StringBuilder sb = new StringBuilder("William Suane");
+        sb.append(" DevDojo").append(" Academy");
+        sb.reverse();
+        sb.reverse();
+        sb.delete(0,3);
+        System.out.println(sb);
+    }
+}
