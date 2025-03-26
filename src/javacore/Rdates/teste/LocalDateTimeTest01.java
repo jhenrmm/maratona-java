d5e46caa885a962529cfae886d190e570ab81c2e
João Henrique <jhenrmm@gmail.com>
2025-03-26 16:30:33 -0300

    Learned about LocalDate, LocalTime, LocalDateTime, Instant, Duration

---
+++ src/javacore/Rdates/teste/LocalDateTimeTest01.java
@@ -0,0 +1,11 @@
+package javacore.Rdates.teste;
+
+import java.time.LocalDateTime;
+
+public class LocalDateTimeTest01 {
+    public static void main(String[] args) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
+        System.out.println(localDateTime.getMonth());
+    }
+}
---
+++ src/javacore/Rdates/teste/LocalTimeTest01.java
@@ -0,0 +1,17 @@
+package javacore.Rdates.teste;
+
+import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+
+public class LocalTimeTest01 {
+    public static void main(String[] args) {
+        LocalTime time = LocalTime.of(23, 32, 12);
+        LocalTime timeNow = LocalTime.now();
+        System.out.println(time);
+        System.out.println(timeNow);
+        System.out.println(timeNow.getHour());
+        System.out.println(timeNow.getMinute());
+        System.out.println(timeNow.getSecond());
+        System.out.println(timeNow.get(ChronoField.HOUR_OF_AMPM));
+    }
+}
