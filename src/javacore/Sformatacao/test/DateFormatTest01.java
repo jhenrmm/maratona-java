10f98b7e5bde17b948311bf7405c6fddfee8c149
João Henrique <jhenrmm@gmail.com>
2025-03-27 16:57:11 -0300

    Learned about TemporatlAdjusters, TemporalAdjuster, ChronoUnit, Period

--- src/javacore/Sformatacao/test/DateFormatTest01.java
+++ src/javacore/Sformatacao/test/DateFormatTest01.java
@@ -2,8 +2,9 @@
 
 import java.text.DateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
-public class DateFormatTest01 {
+    public class DateFormatTest01 {
     public static void main(String[] args) {
         Calendar calendar = Calendar.getInstance();
         DateFormat[] df = new DateFormat[7];
@@ -19,5 +20,26 @@ public class DateFormatTest01 {
             System.out.println(dateFormate.format(calendar.getTime()));
         }
     }
-}
+
+        public static class LocaleTest01 {
+            public static void main(String[] args) {
+                Locale localeItaly = new Locale("it", "IT");
+                Locale localeCH = new Locale("it", "CH");
+                Locale localeIndia = new Locale("hi", "IN");
+                Locale localeJapao = new Locale("ja", "JP");
+                Calendar calendar = Calendar.getInstance();
+                DateFormat df1 = DateFormat.getDateInstance(DateFormat.FULL, localeItaly);
+                DateFormat df2 = DateFormat.getDateInstance(DateFormat.FULL, localeCH);
+                DateFormat df3 = DateFormat.getDateInstance(DateFormat.FULL, localeIndia);
+                DateFormat df4 = DateFormat.getDateInstance(DateFormat.FULL, localeJapao);
+                System.out.println("Itália: "+ df1.format(calendar.getTime()));
+                System.out.println("Suiça: "+df2.format(calendar.getTime()));
+                System.out.println("India: "+df3.format(calendar.getTime()));
+                System.out.println("Japão: "+df4.format(calendar.getTime()));
+
+                System.out.println(localeItaly.getDisplayCountry(localeItaly));
+                System.out.println(localeJapao.getDisplayCountry(localeJapao));
+            }
+        }
+    }
 
