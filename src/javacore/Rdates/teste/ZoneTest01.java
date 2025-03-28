580f358abe5e5d019d7567113fca1ec94691c929
João Henrique <jhenrmm@gmail.com>
2025-03-28 16:45:40 -0300

    Learned about ZonedDataTime, DataTimeFormatter and ResourceBundle

---
+++ src/javacore/Rdates/teste/ZoneTest01.java
@@ -0,0 +1,18 @@
+package javacore.Rdates.teste;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.Map;
+
+public class ZoneTest01 {
+    public static void main(String[] args) {
+        Map<String, String> shortIds = ZoneId.SHORT_IDS;
+        System.out.println(shortIds);
+        System.out.println(ZoneId.systemDefault());
+        ZoneId tokyoZone = ZoneId.of("Asia/Tokyo");
+        System.out.println(tokyoZone);
+        System.out.println(ZoneOffset.MAX);
+        System.out.println(ZoneOffset.MIN);
+    }
+}
