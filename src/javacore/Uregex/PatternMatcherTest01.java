81b7665108d20afd89692c75523aacce9087db5b
João Henrique <jhenrmm@gmail.com>
2025-03-29 22:03:35 -0300

    Learned about Regex - Part 01

---
+++ src/javacore/Uregex/PatternMatcherTest01.java
@@ -0,0 +1,20 @@
+package javacore.Uregex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest01 {
+    public static void main(String[] args) {
+        String regex = "ab";
+        String texto = "abaaba";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto);
+        System.out.println("Texto: "+texto);
+        System.out.println("Indice: 0123456789");
+        System.out.println("regex "+regex);
+        System.out.println("Posições encontradas");
+        while(matcher.find()){
+            System.out.print(matcher.start()+" ");
+        }
+    }
+}
---
+++ src/javacore/Uregex/PatternMatcherTest03.java
@@ -0,0 +1,31 @@
+package javacore.Uregex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest03 {
+    public static void main(String[] args) {
+        // \d = todos os numeros
+        // \D = tudo o que não for digito
+        // \s = espaços em branco \t \n \f \r
+        // \S = todos os caracteres menos os espacos brancos
+        // \w = a-Z A-Z, digitos, _ (todos esses valores são verificados)
+        // \W = Tudo o que não for incluso no \w
+        // [] = encontra todos os termos dentro da lista
+//        String regex = "[a-zA-C]";
+        String regex = "0[xX][0-9a-fA-F]";
+        String texto = "abaaba";
+        String texto2 = "12 0x 0x 0xFFABC 0x109 0x1";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto2);
+        System.out.println("Texto: "+texto2);
+        System.out.println("Indice: 0123456789");
+        System.out.println("regex "+regex);
+        System.out.println("Posições encontradas");
+        while(matcher.find()) {
+            System.out.print(matcher.start() + " "+matcher.group()+"\n");
+        }
+        int numeroHex = 0x109;
+        System.out.println(numeroHex);
+    }
+}
