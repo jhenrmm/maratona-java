223d059766dd2e8985f0edeeb88fbaa5c7548bbd
João Henrique <jhenrmm@gmail.com>
2025-04-21 20:28:29 -0300

    Learned about NavigableSet, TreeMap, Queue, PriorityQueue, Wildcard

---
+++ src/javacore/Ycolecoes/test/QueueTest02.java
@@ -0,0 +1,21 @@
+package javacore.Ycolecoes.test;
+
+import javacore.Ycolecoes.dominio.Manga;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class QueueTest02 {
+    public static void main(String[] args) {
+        Queue<Manga> mangas = new PriorityQueue<>(new MangaPrecoComparator().reversed());
+        mangas.add(new Manga(4L, "Berserk", 19.9, 0) );
+        mangas.add(new Manga(1L, "Pokemon", 5, 3));
+        mangas.add(new Manga(3L, "Attack on Titan", 3.2, 2));
+        mangas.add(new Manga(2L, "Dragon Ball Z", 11.20, 0));
+        mangas.add(new Manga(10L, "Aaragon", 11.20, 1));
+
+        while(!mangas.isEmpty()){
+            System.out.println(mangas.poll());
+        }
+    }
+}
