44ddd17697dada39024b5a3823e743817b53d40b
João Henrique <jhenrmm@gmail.com>
2025-04-24 17:55:51 -0300

    Learned about Method References and Optional

---
+++ src/javacore/ZZDOptional/test/OptionalTest02.java
@@ -0,0 +1,20 @@
+package javacore.ZZDOptional.test;
+
+import javacore.ZZDOptional.dominio.Manga;
+import javacore.ZZDOptional.repositorio.MangaRepositorio;
+
+import java.util.Optional;
+
+public class OptionalTest02 {
+    public static void main(String[] args) {
+        Optional<Manga> mangaByTitle = MangaRepositorio.findByTitle("Boku no Hero");
+        mangaByTitle.ifPresent(m -> m.setTitle("Boku no Hero 2"));
+        System.out.println(mangaByTitle);
+
+        Manga mangaById = MangaRepositorio.findById(2).orElseThrow(IllegalArgumentException::new);
+        System.out.println(mangaById);
+
+        Manga newManga =  MangaRepositorio.findByTitle("Drifters").orElseGet(() -> new Manga(3, "Drifters", 20));
+        System.out.println(newManga);
+    }
+}
